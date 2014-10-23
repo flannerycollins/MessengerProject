8dcce22b375bca6f3053499936e27bcf64152f8e
Kira <sitzkoff@mail.umw.edu>
2014-10-23 14:00:57 -0400

    Update NewThing.java

--- NewThing.java
+++ NewThing.java
@@ -1,1 +1,11 @@
 //NEW THING!
+
+public class NewThing{
+  public NewThing(){
+    System.out.println("OMG!");
+  }
+  
+  public static void main(String args[]){
+    NewThing newness = new NewThing();
+  }
+}
