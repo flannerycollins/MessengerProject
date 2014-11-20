23f3c43e7e130c28c46ad52693c6cd3fa1ff5623
Kira <Raikit@users.noreply.github.com>
2014-11-20 13:31:44 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Kira
+ */
 public class User {
     
     public void User(){
@@ -9,6 +20,14 @@ public class User {
     public void User(String uN, int uID){
         
         name = uN;
+        iD = uID;
+    }
+    
+    public void setUN(String uN){
+        name = uN;
+    }
+    
+    public void setID(int uID){
         iD = uID;
     }
     
